f9f04ce0ce9b1ae1b03a107b38f1416cedebb70c
Ihimakshisharma <sharmahimakshi7@gmail.com>
2025-12-01 14:45:45 +0530

    Implement input validation in FCFS scheduling
    Added input validation for process count and negative values.

--- fcfs.cpp
+++ fcfs.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 void fcfs(int pid[], int at[], int bt[], int n) {
 
+    if (n <= 0) {
+        cout << "No processes to schedule.\n";
+        return;
+    }
+    if (n > 20) n = 20;
+
+    for (int i = 0; i < n; i++) {
+        if (at[i] < 0) at[i] = 0;
+        if (bt[i] < 0) bt[i] = 0;
+    }
+
     int ct[20], tat[20], wt[20];
 
     for (int i = 0; i < n - 1; i++) {
@@ -29,6 +40,8 @@ void fcfs(int pid[], int at[], int bt[], int n) {
     int gantt_pid[40];
     int gantt_end[40];
     int gcount = 0;
+    int total_tat = 0;
+    int total_wt = 0;
 
     for (int i = 0; i < n; i++) {
 
@@ -43,6 +56,9 @@ void fcfs(int pid[], int at[], int bt[], int n) {
         ct[i] = time;
         tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
+
+        total_tat += tat[i];
+        total_wt += wt[i];
 
         gantt_pid[gcount] = pid[i];
         gantt_end[gcount] = time;
@@ -82,4 +98,12 @@ void fcfs(int pid[], int at[], int bt[], int n) {
         else cout << " " << t;
     }
     cout << "\n";
+
+    double avg_tat = (double)total_tat / n;
+    double avg_wt = (double)total_wt / n;
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << "\nAverage Turnaround Time: " << avg_tat << "\n";
+    cout << "Average Waiting Time: " << avg_wt << "\n";
 }
