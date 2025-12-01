ffefebddb8db2ef008dc349fe955059b32baa8cd
Ihimakshisharma <sharmahimakshi7@gmail.com>
2025-12-01 19:34:02 +0530

    Add visualization header with drawing functions

---
+++ visualization.h
@@ -0,0 +1,7 @@
+#ifndef VISUALIZATION_H
+#define VISUALIZATION_H
+
+void drawGantt(int pid[], int end[], int count);
+void drawTable(int pid[], int at[], int bt[], int ct[], int tat[], int wt[], int n);
+
+#endif
